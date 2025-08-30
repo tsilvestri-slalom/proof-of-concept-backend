a45d993cab03b9085cd61e5a98d63ea04fba886f
Tony Silvestri <tonys@slalom.com>
2025-08-30 16:15:55 -0400

    Initial Spring Boot API setup

---
+++ src/main/java/com/example/hello_api/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.hello_api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class HelloController {
+
+    @PostMapping("/hello")
+    public HelloResponse hello(@RequestBody HelloRequest request) {
+        return new HelloResponse("Hello, " + request.getName());
+    }
+    
+    static class HelloRequest {
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    
+    static class HelloResponse {
+        private String message;
+        
+        public HelloResponse(String message) {
+            this.message = message;
+        }
+        
+        public String getMessage() {
+            return message;
+        }
+        
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
